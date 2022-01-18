b1a33d69b413d679b0dd19230baf4d3d8557c000
Rafael Kauã Chicovis <rafaelkaua97@gmail.com>
2022-01-18 18:45:08 -0300

    CUR-12 added Aula entity, migration e modulo docs (#12)

---
+++ Migrations/20220118214326_AddedAula.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RKC.Cursos.Migrations
+{
+    public partial class AddedAula : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aulas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IdModulo = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    DataAcontecer = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aulas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aulas");
+        }
+    }
+}
